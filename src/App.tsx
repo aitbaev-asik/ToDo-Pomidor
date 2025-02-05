96be20d0b6b1b3fcf07378f064d504920760c9b1
Aslanbek Aitbaev <aslanbek2004.2019@gmail.com>
2025-02-05 11:34:38 +0500

    clean Libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { TaskList } from './components/TaskList';
 import { PomodoroTimer } from './components/PomodoroTimer';
 import { ThemeToggle } from './components/ThemeToggle';
